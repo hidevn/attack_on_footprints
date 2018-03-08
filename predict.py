9544f7328526e0123cd6819aff8f55eae3a77ef2
DESKTOP-JCHPSP3\ASUS <hidevn@yahoo.com>
2018-03-08 10:57:07 +0700

    predict

--- predict.py
+++ predict.py
@@ -1,8 +1,5 @@
 import numpy as np
-import scipy
 from matplotlib import pyplot as plt
-from matplotlib import image as img
-import rasterio
 import utils
 import model
 
@@ -12,40 +9,19 @@ def normalize(batch):
         batch = batch/std[np.newaxis, np.newaxis, np.newaxis, :]
         return batch
 
-def predict(path, x_start = 0, y_start = 0, m = None):
-    img = utils.read_single_image(path)
-    target_size = 256
+def predict(path, m = None, threshold = None):
+    i = utils.read_single_image(path)
     if m == None:
-        m = model.fcn_resnet((256, 256, 3), 6)
-        m.load_weights('./model.h5')
-    nb_rows, nb_cols, _ = np.shape(img)
-    # img = scipy.misc.imresize(img, (512, 512, 3))
-    plt.imshow(img)
-    '''
-    result = np.zeros((nb_rows, nb_cols, 6))
-    for row_begin in range(0, nb_rows, target_size):
-        for col_begin in range(0, nb_cols, target_size):
-            row_end = row_begin + target_size
-            col_end = col_begin + target_size
-            if row_end <= nb_rows and col_end <= nb_cols:
-                window = [[row_begin, row_end], [col_begin, col_end]]
-                i = utils.read_img_window(img, window)
-                i = np.expand_dims(i, axis = 0)
-                i = normalize(i)
-                result[window[0][0]:window[0][1], window[1][0]:window[1][1], :] = m.predict(i, 1)[0]
-                result = np.argmax(result, axis = 2)
-    '''
-    window = [[x_start, x_start+256], [y_start, y_start+256]]
-    i = utils.read_img_window(img, window)
+        m = model.fcn_resnet((256, 256, 3), 1)
+        m.load_weights('./weights.45-0.32.h5')
+    nb_rows, nb_cols, _ = np.shape(i)
     plt.imshow(i)
     i = np.expand_dims(i, axis = 0)
     i = normalize(i)
     result = m.predict(i, 1)[0]
-    # result = np.argmax(result, axis = 2)
     plt.figure(2)
-    plt.imshow(result[:, :, 1])
-    return result[:, :, 1]
-
-    
-                
-    
+    if threshold == None:
+        plt.imshow(result[:, :, 0])
+    else:
+        plt.imshow(result[:, :, 0] > threshold)
+    return result[:, :, 0]
